0e659e19ad7307fe75ccc9fc7f4008e5c0bb5fbc
Dhananjay Arora <arora.dhananjay@gmail.com>
2019-06-05 00:18:05 -0400

    Delete Question6.py

--- Question6.py
+++
@@ -1,9 +0,0 @@
-# Write a Python program to demonstrate that a function can be defined inside another function and a function can be passed as parameter.
-def outerFunction(parameter):
-    def innerFunction(parameter):
-        print(parameter)
-    innerFunction(parameter)
-def function(outerFunction,parameter):
-    outerFunction(parameter)
-parameter= "Welcome to Python Programming"
-function(outerFunction,parameter)
